3026ec9501cf46fc97085a3f0c5561ec2114a4c9
Luciano Mariño <jmarino@falabella.com.ar>
2021-04-26 16:38:53 -0300

    .

--- src/dependencies.php
+++ src/dependencies.php
@@ -18,14 +18,14 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
-// Service factory for the ORM
-$capsule = new \Illuminate\Database\Capsule\Manager;
-$capsule->addConnection($container['settings']['db']);
-
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
-
 $container['db'] = function ($container) {
+	// Service factory for the ORM
+	$capsule = new \Illuminate\Database\Capsule\Manager;
+	$capsule->addConnection($container['settings']['db']);
+
+	$capsule->setAsGlobal();
+	$capsule->bootEloquent();
+
     return $capsule;
 };
 
