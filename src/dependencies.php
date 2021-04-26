eb46fcaed1b8c931fa718eeb194160ec6e4bc6af
Luciano Mariño <jmarino@falabella.com.ar>
2021-04-26 16:40:21 -0300

    .

--- src/dependencies.php
+++ src/dependencies.php
@@ -25,14 +25,7 @@ $capsule->addConnection($container['settings']['db']);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
-$container['db'] = function ($container) {
-	// Service factory for the ORM
-	$capsule = new \Illuminate\Database\Capsule\Manager;
-	$capsule->addConnection($container['settings']['db']);
-
-	$capsule->setAsGlobal();
-	$capsule->bootEloquent();
-
+$container['db'] = function ($capsule) {
     return $capsule;
 };
 
